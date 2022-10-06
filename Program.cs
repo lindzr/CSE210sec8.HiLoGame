165fe818446eb423117787191f03f7a72bcc19c6
lindzr <munnsli@byui.edu>
2022-10-06 15:02:06 -0600

    Structure main program

--- Program.cs
+++ Program.cs
@@ -9,20 +9,17 @@
         // Loop continuously until the deck no longer has any cards
         do 
         {   
+            string currentCard = deckOfCards.DrawRandomCard();
             Console.WriteLine($"The card is: {currentCard}");
             Console.WriteLine("Higher or Lower [h/l]? ");
             string userGuess = Console.ReadLine() ?? "";
+            Card newCard = deckOfCards.DrawRandomCard();
             Console.WriteLine($"Next card was: {newCard}");
-            Console.WriteLine($"Your score is: {score}");
+            Console.WriteLine($"Your score is: {calculateNewScore(currentCard,newCard,userGuess)}");
             Console.WriteLine("Play again? [y/n] ");
             play = Console.ReadLine() ?? "";
-            // Draw a random card from the deck
-            Card newCard = deckOfCards.DrawRandomCard();
-            
-            // Print the card description to the console
-            Console.WriteLine(randomCard.Description());
 
-        } while (deckOfCards.HasMoreCards());
+        } while (deckOfCards.HasMoreCards() && play == "y");
 
         Console.WriteLine("");
     }
